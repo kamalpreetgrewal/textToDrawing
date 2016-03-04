119dd57aa652bd861132dfcf61b2e10b67b57641
kamalpreetgrewal <grewalkamal005@gmail.com>
2016-03-04 22:29:51 +0530

    enable random argument input for wall entity

--- main.cpp
+++ main.cpp
@@ -34,41 +34,103 @@ int main()
     ofile.close();
 
     // defining regexp to save only numbers in the parsed data in a list
-    QRegularExpression exp1("^[0-9]*$");
-    QVector<QString> v;
+    //QRegularExpression exp1("^[0-9]*$");
+    QVector<QVector<QString>*> bigV;
+    QVector<QString>* v;
     QString c;
+    int count = -1;
+
     ofile.open(QIODevice::ReadOnly);
     // read whole content
-    while (!ofile.atEnd()) {
+    while (!ofile.atEnd())
+    {
         c = ofile.readLine();
-        if (c.contains(exp1))
+
+        if(c == "wall\n")
         {
-            v.append(c);
+            ++count;
+            v = new QVector<QString>(5);
+            (*v)[0] = c;
+            qDebug() << "wall";
         }
-        qDebug() << v;
+        if(c == "ball\n")
+        {
+            ++count;
+            v = new QVector<QString>(4);
+            (*v)[0] = c;
+            qDebug() << "ball";
+        }
+        if(c == "l\n" && count >= 0 && v != NULL)
+        {
+            qDebug() << "l";
+            (*v)[1] = ofile.readLine();
+
+        }
+        if(c == "h\n" && count >= 0 && v != NULL)
+        {
+            qDebug() << "h";
+            (*v)[2] = ofile.readLine();
+
+        }
+        if(c == "bx\n" && count >= 0 && v != NULL)
+        {
+            (*v)[3] = ofile.readLine();
+        }
+        if(c == "by\n" && count >= 0 && v != NULL)
+        {
+            (*v)[4] = ofile.readLine();
+        }
+        if(c == "r\n" && count >= 0 && v != NULL)
+        {
+            (*v)[1] = ofile.readLine();
+        }
+        if(c == "cx\n" && count >= 0 && v != NULL)
+        {
+            (*v)[2] = ofile.readLine();
+        }
+        if(c == "cy\n" && count >= 0 && v != NULL)
+        {
+            (*v)[3] = ofile.readLine();
+        }
+        if(c == "\n" && count >= 0 && v != NULL)
+        {
+            qDebug() << "v : " << v;
+            bigV.append(v);
+            //delete v;
+            //v = 0;
+        }
     }
+    qDebug() << bigV.count();
 
     ofile.close();
 
     // creating the entity class' object and calling its function
     entity e;
-    QString str = "wall";
-    int num_walls;
-    num_walls = content.count(str);
 
     DL_Dxf *abc = e.return_dxf();
     DL_WriterA *def = e.return_dw();
     e.startDXF(abc, def);
-    int j=0;
-    for (int i = 0; i<num_walls;i++)
+
+    for (int i = 0; i < bigV.size(); ++i)
     {
-        e.createWall(abc, def, v.at(j).toFloat(), v.at(j+1).toFloat(),
-                     v.at(j+2).toFloat(), v.at(j+3).toFloat(),
-                     "wall", 256, 1, "CONINUOUS");
-        j=j+5;
+        QVector<QString>* ab = bigV.at(i);
+        if (ab->at(0) == "wall\n")
+        {
+            int j = 1;
+            e.createWall(abc, def, ab->at(j).toFloat(), ab->at(j+1).toFloat(),
+                         ab->at(j+2).toFloat(), ab->at(j+3).toFloat(),
+                         "wall", 256, 1, "CONINUOUS");
+        }
+        if (ab->at(0) == "ball\n")
+        {
+            int j = 1;
+            //            e.createWall(abc, def, ab->at(j).toFloat(), ab->at(j+1).toFloat(),
+            //                                 ab->at(j+2).toFloat(), ab->at(j+3).toFloat(),
+            //                                 "wall", 256, 1, "CONINUOUS");
+        }
     }
-    e.createFlange(abc, def, 20, 60, 100);
-    //w.createFlower(abc,def,fl.at(0),fl.);
+
+    //    e.createFlange(abc, def, 20, 60, 100);
     e.closeDXF(def);
 
     QProcess::execute("librecad myfile.dxf");
